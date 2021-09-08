c8c449e694e714a4ed5e3675c9d3d0b8ac8b2de8
warabanshi <warabanshi@gmail.com>
2021-09-08 12:19:53 +0900

    Added commands

--- rfind/cmd.py
+++ rfind/cmd.py
@@ -8,47 +8,63 @@ URL = 'http://localhost:8000/'
 HEADERS = {'content-type': 'application/json'}
 
 
-def run_command(command: str) -> None:
-    r = subprocess.call(command)
-
-    if r != 0:
-        raise Exception(f'command failed. code={r}')
-
-
 def call_server(payload=None):
     r = requests.post(URL, data=json.dumps(payload), headers=HEADERS)
 
     print(f"send payload: {payload}")
     print(f"response = {r.text}")
 
+    return r
+
+
+def run_command(command: str) -> None:
+    args = command.split(' ')
+    r = subprocess.run(args)
+
+    if r.returncode != 0:
+        raise Exception(f'command failed. code={r}')
+
 
 def register(command: str) -> None:
     try:
+        print(command)
         run_command(command)
-    except FileNotFoundError as e:
+    except FileNotFoundError as e1:
         # raise from r
-        print('invalid command')
+        print(f'invalid command: {e1}')
         return
     except Exception as e2:
-        print('command failed')
+        print(f'command failed: {e2}')
         return
 
     payload = {'instruction': 'register', 'body': command}
     call_server(payload=payload)
 
 
+def flush() -> None:
+    payload = {'instruction': 'flush'}
+    call_server(payload)
+
+
 def get_all() -> None:
     payload = {'instruction': 'get_all'}
-    call_server(payload)
+    r = call_server(payload)
+
+    results = json.loads(r.text)
+    for i in range(len(results)):
+        print(f"{i+1}: {results[i]}")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-r', '--register', help="register a command")
+    parser.add_argument('-f', '--flush', help="flush registered data", action='store_true')
 
     args = parser.parse_args()
 
     if args.register:
         register(args.register)
+    elif args.flush:
+        flush()
     else:
         get_all()
