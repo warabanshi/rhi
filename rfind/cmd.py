ef09d27b2fb41167b5ba5c07f3e14c4de0eb73cb
warabanshi <warabanshi@gmail.com>
2021-09-17 01:22:30 +0900

    input history from stdin

--- rfind/cmd.py
+++ rfind/cmd.py
@@ -1,17 +1,24 @@
 import argparse
+import io
 import json
 import requests
+import os
 import subprocess
+import sys
+
+from typing import List
 
 
 URL = 'http://localhost:8000/'
 HEADERS = {'content-type': 'application/json'}
+HISTTIMEFORMAT = os.getenv('HISTTIMEFORMAT')
 
 
 def call_server(payload=None):
     r = requests.post(URL, data=json.dumps(payload), headers=HEADERS)
 
     print(f"send payload: {payload}")
+    print(f"response = {r}")
     print(f"response = {r.text}")
 
     return r
@@ -19,10 +26,13 @@ def call_server(payload=None):
 
 def run_command(command: str) -> None:
     args = command.split(' ')
-    r = subprocess.run(args)
+
+    r = subprocess.run(args, capture_output=True)
 
     if r.returncode != 0:
         raise Exception(f'command failed. code={r}')
+
+    print(r)
 
 
 def add(command: str) -> None:
@@ -62,16 +72,40 @@ def get_all() -> None:
         print(f"{i+1}: {results[i]}")
 
 
+def cleanup_input(inputs: io.TextIOWrapper, rownum: int=None) -> str:
+    lines = [s.lstrip(' ').lstrip('0123456789').strip() for s in inputs.readlines()]
+
+    if len(lines) == 0:
+        raise Exception("There's no valid lines in input")
+    
+    if rownum is None:
+        return lines[max(len(lines)-2, 0)]
+
+    if 1 <= rownum <= len(lines):
+        return lines[rownum-1]
+
+    raise Exception(f'Invalid rownum is specified. rownum={rownum}')
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-a', '--add', help="add a command")
+    parser.add_argument('-a', '--add', nargs='?', const=sys.stdin, help="add a command (expects stdin)")
+    parser.add_argument('-n', '--num', type=int, default=0, help="history number")
     parser.add_argument('-f', '--flush', help="flush registered data", action='store_true')
-    parser.add_argument('-r', '--run', help="add a command")
+    parser.add_argument('-r', '--run', help="run a specified command")
 
     args = parser.parse_args()
 
+    command = None
     if args.add:
-        add(args.add)
+        if sys.stdin.isatty():
+            print('stdin waits input')
+            sys.exit()
+        else:
+            command = cleanup_input(args.add, args.num)
+
+    if command:
+        add(command)
     elif args.flush:
         flush()
     elif args.run:
