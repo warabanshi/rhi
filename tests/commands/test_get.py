1815f0ae264832498b10d897735e161fa9157159
warabanshi <warabanshi@gmail.com>
2021-09-29 23:01:40 +0900

    Modified "get" instruction output #8

---
+++ tests/commands/test_get.py
@@ -0,0 +1,56 @@
+import json
+import pytest
+
+import requests.models
+
+from unittest.mock import patch
+
+from rhi.commands.get import Get
+
+
+@patch('rhi.commands.get.Get.__init__')
+@patch('rhi.commands.get.Get.call_get')
+def test_get_all_output(mock_call_get, mock_get_init):
+    res = requests.models.Response()
+    res.status_code = 200
+    res.encoding = 'utf-8'
+    res._content = bytes(json.dumps({
+        'result': [
+            {'command': 'ls -alh', 'message': ''},
+            {'command': 'cat /etc/SUSE-brand', 'message': 'opensuse version file'},
+        ]}), 'utf-8')
+
+    mock_get_init.return_value = None
+    mock_call_get.return_value = res
+
+    expected = [
+        ' 1: ls -alh', 
+        ' 2: cat /etc/SUSE-brand   # opensuse version file',
+    ]
+
+    get = Get()
+    result = get.get_all()
+
+    assert result == expected
+
+@patch('rhi.commands.get.Get.__init__')
+@patch('rhi.commands.get.Get.call_get')
+def test_get_single_output(mock_call_get, mock_get_init):
+    res = requests.models.Response()
+    res.status_code = 200
+    res.encoding = 'utf-8'
+    res._content = bytes(json.dumps({
+        'result': {'command': 'cat /etc/SUSE-brand', 'message': 'opensuse version file'}
+    }), 'utf-8')
+
+    mock_get_init.return_value = None
+    mock_call_get.return_value = res
+
+    expected = ['cat /etc/SUSE-brand   # opensuse version file']
+
+    get = Get()
+    get.num = 2
+    result = get.get_single()
+
+    assert result == expected
+
