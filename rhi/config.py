76df57c5c49b0480f7822a22184e20d7e3dcf533
warabanshi <warabanshi@gmail.com>
2021-09-29 23:56:46 +0900

    Fixed format by black

--- rhi/config.py
+++ rhi/config.py
@@ -2,6 +2,6 @@ import os
 
 ENVIRONMENT = os.getenv("ENV", "dev")
 URL = "http://localhost:20080"
-#URL = "http://192.168.1.250:20080"
+# URL = "http://192.168.1.250:20080"
 HEADERS = {"content-type": "application/json"}
 HISTTIMEFORMAT = os.getenv("HISTTIMEFORMAT")
