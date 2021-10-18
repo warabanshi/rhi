5cf59bef9590004bcd65eff88f41affb76fab1a3
warabanshi <warabanshi@gmail.com>
2021-10-19 00:21:36 +0900

    Added version command #25

---
+++ rhi/commands/version.py
@@ -0,0 +1,10 @@
+import pkg_resources
+
+from rhi.commands.command import Command
+
+
+class Version(Command):
+
+    def invoke(self):
+        ver = pkg_resources.get_distribution('rhi').version
+        print(f'rhi-{ver}')
